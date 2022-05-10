83e54066ca2982712617a9c4818efc8331216738
zhengkeyan <zhengkeyan@bjca.org.cn>
2022-05-10 17:17:40 +0800

    改进

---
+++ SharpXDecrypt/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+
+
+namespace SharpXDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             Console.WriteLine();
+             Console.WriteLine("Xshell Info!");
+             Console.WriteLine();
+             if (args.Length == 1)
+             {
+                 XClass.Decrypt(args[0]);
+             }
+             else
+             {
+                 XClass.Decrypt();
+             }
+             Console.WriteLine("[*] read done!");
+//             Console.ReadKey();
+
+        }
+    }   
+}
